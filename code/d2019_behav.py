3bba399f1d3650575f106ddb44b92c8cd822a9d5
huanhwwang <huan.hw.wang@gmail.com>
2020-11-01 13:44:36 -0800

    new scripts for modeling

--- code/d2019_behav.py
+++ code/d2019_behav.py
@@ -12,7 +12,7 @@ import glob
 from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from scipy.stats import t
 
 datadir='/Users/huanwang/GoogleDrive/Stanford/1st_yr/251/valence-control/data/'
 
@@ -25,15 +25,22 @@ dt_lg_stat1=dt.groupby(['subject','feedback']).mean()['latent_guess'].unstack()
 dt_lg_stat2=dt.groupby(['subject','condition','feedback']).mean()['latent_guess'].unstack().unstack()
 
 behav_stat1=stats.ttest_rel(dt_lg_stat1[0],dt_lg_stat1[1])  #different from reported stats "t(71) = 16.82, p < .0001"
+behav_stat1_d=(dt_lg_stat1.mean().diff()[1])/(np.sqrt(sum(dt_lg_stat1.std().apply(np.sqrt))/2))
+
+#behav_stat1_ci=t.interval(0.95,len(dt_lg_stat1)-1,loc=behav_stat1_d, scale=dt_lg_stat1.sem())
+
 
 behav_stat2=[stats.ttest_rel(dt_lg_stat2[0][1],dt_lg_stat2[1][1]),\
              stats.ttest_rel(dt_lg_stat2[1][2],dt_lg_stat2[0][2]),\
              stats.ttest_rel(dt_lg_stat2[0][3],dt_lg_stat2[1][3])]
-dt_fig2a=pd.DataFrame(dt_lg_stat2.mean()).reset_index()
+
+dt_fig2a=dt_lg_stat2.melt(value_name="latent_guess",ignore_index=False)
 
 plt.figure(figsize=[10,3])
 plt.ylim(0,1)
 plt.yticks([0,0.5,1])
-
-ax=sns.barplot(x="condition",y=0,hue="feedback",hue_order=[1,0],data=dt_fig2a)
+ax=sns.barplot(x="condition",y="latent_guess",hue="feedback",hue_order=[1,0],data=dt_fig2a,ci=68,capsize=.02)
 ax.figure.savefig(datadir+'../../dorfman2019/code/Fig2a.jpg')
+
+
+
